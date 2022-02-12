dd32149486c6b4babbbd59dd04c7308c39df08f1
DESKTOP-99N202T <wlodarczyk223@wp.pl>
2022-02-12 18:41:48 +0100

    1.4 stale 2 Dodanie final przy niemutowalnych atrybutach klas

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-		public DefaultCountingOutRhymer defCountOutRhym = new DefaultCountingOutRhymer();
+		public final DefaultCountingOutRhymer defCountOutRhym = new DefaultCountingOutRhymer();
 	
 		@Override
 		public int countOut() {
